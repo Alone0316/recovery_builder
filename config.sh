9afd1b7b4f6fecf2d57232544a7267ea117af4b0
Alone0316 <70204635+Alone0316@users.noreply.github.com>
2022-06-07 15:35:43 +0530

    bruh

---
+++ config.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Device
+export FOX_BRANCH="fox_11.0"
+export DT_LINK="https://github.com/Alone0316/recovery_samsung_a52sxq.git"
+
+export DEVICE="a52sxq"
+export OEM="samsung"
+
+# Build Target
+## "recoveryimage" - for A-Only Devices without using Vendor Boot
+## "bootimage" - for A/B devices without recovery partition (and without vendor boot)
+## "vendorbootimage" - for devices Using vendor boot for the recovery ramdisk (Usually for devices shipped with Android 12 or higher)
+
+export TARGET="recoveryimage"
+
+export OUTPUT="OrangeFox*.zip"
+
+# Kernel Source
+# Uncomment the next line if you want to clone a kernel source.
+#export KERNEL_SOURCE="https://gitlab.com/OrangeFox/kernel/mojito.git"
+#export PLATFORM="sm6150" # Leave it commented if you want to clone the kernel to kernel/$OEM/$DEVICE
+
+# Magisk
+## Use the Latest Release of Magisk for the OrangeFox addon
+OF_USE_LATEST_MAGISK=true
+
+# Not Recommended to Change
+export USE_CCACHE=1
+export CCACHE_SIZE="50G"
+export CCACHE_DIR="$HOME/work/.ccache"
+export J_VAL=16
+
+if [ ! -z "$PLATFORM" ]; then
+    export KERNEL_PATH="kernel/$OEM/$PLATFORM"
+else
+    export KERNEL_PATH="kernel/$OEM/$DEVICE"
+fi
+export DT_PATH="device/$OEM/$DEVICE"
